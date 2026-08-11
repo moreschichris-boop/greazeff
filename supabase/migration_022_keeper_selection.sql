68d2722b689af0b6da128b33dbbbc9720b4a2e00
moreschichris-boop <moreschichris@gmail.com>
2026-08-11 10:43:07 -0400

    Delete supabase directory

--- supabase/migration_022_keeper_selection.sql
+++
@@ -1,6 +0,0 @@
--- Migration: adds a "selected as keeper" flag to roster_entries, separate
--- from keeper_eligible (which just marks who COULD be kept). This tracks
--- which players an owner actually chose to keep, enforced to at most 2
--- in the admin UI. Safe to run once on your existing Supabase project.
-
-alter table roster_entries add column if not exists keeper_selected boolean default false;

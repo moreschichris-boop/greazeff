6be5d83bb73001cf12ddcb05ebfbe8f17e3c91f8
moreschichris-boop <moreschichris@gmail.com>
2026-08-11 10:48:06 -0400

    Delete migration_029_fix_2025_26_toilet_bowl.sql

--- migration_029_fix_2025_26_toilet_bowl.sql
+++
@@ -1,6 +0,0 @@
--- Correction: 2025-26 Toilet Bowl should be Rob Moreschi, not Cuyler
--- Peragallo. Safe to run once.
-
-update seasons
-set last_place_id = (select id from owners where slug = 'rob-moreschi')
-where year = '2025-26';
